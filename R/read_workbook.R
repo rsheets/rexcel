3a50db186b04373e8e7c4f0bca049f8ac277f501
jennybc <jenny@stat.ubc.ca>
2016-05-30 14:56:12 -0700

    xml2::xml_find_one --> xml_find_first

--- R/read_workbook.R
+++ R/read_workbook.R
@@ -14,7 +14,7 @@ xlsx_read_workbook_sheets <- function(path) {
   ## namespace avoidance in order to handle xlsx like
   ## Ekaterinburg_IP.xlsx from here:
   ## https://github.com/hadley/readxl/issues/80
-  sheets <- xml2::xml_find_one(xml, ".//*[local-name() = 'sheets']")
+  sheets <- xml2::xml_find_first(xml, ".//*[local-name() = 'sheets']")
   sheets_att <- lapply(xml2::xml_contents(sheets), xml_attrs_list)
   tibble::data_frame(
     name = vcapply2(sheets_att, "name"),
@@ -22,6 +22,96 @@ xlsx_read_workbook_sheets <- function(path) {
     sheet_id = as.integer(vcapply2(sheets_att, "sheetId")),
     id = vcapply2(sheets_att, "id")
   )
+}
+
+xlsx_read_workbook_defined_names <- function(path) {
+  xml <- xlsx_read_file(path, "xl/workbook.xml")
+
+  ## 18.14.5 definedName
+  ## 18.14.6 definedNames
+
+  ## the definedName nodes can have different XML structure
+  ## Rich had one in mind during his initial pass (or just worked from spec)
+  ## --> all info found in attributes (name, refersTo, sheedId)
+  ## which motivated
+  ## xlsx_ct_external_defined_names()
+  ## Jenny sees a different structure in the example sheet she created
+  ## inst/sheets/defined_names.xlsx
+  ## --> node value gives cell ref (Rich found this in refersTo attr)
+  ## --> attributes refersTo, sheetId don't even exist
+  ##
+  ## this is an attempt to accomodate both but Jenny doesn't have an actual
+  ## example of the first form to look at
+  dn_nodes <- xml2::xml_find_all(xml, "//*[local-name() = 'definedName']")
+  ## why do I write this weird XPath?
+  ## namespace avoidance in order to handle xlsx like
+  ## Ekaterinburg_IP.xlsx from here:
+  ## https://github.com/hadley/readxl/issues/80
+  if (length(dn_nodes) == 0) {
+    return(NULL)
+  }
+  dn_att <- lapply(dn_nodes, xml_attrs_list)
+
+  ## this is where the spec suggests you will find the cell refs/ranges
+  refers_to <- vcapply2(dn_att, "refersTo")
+  ## and yet Jenny finds them as node text ...
+  if (all(is.na(refers_to))) {
+    refers_to <- xml2::xml_text(dn_nodes)
+  }
+
+  ## may just be NAs
+  sheet_id <- as.integer(vcapply2(dn_att, "sheetId"))
+
+  tibble::data_frame(
+     name = vcapply2(dn_att, "name"),
+     refers_to,
+     sheet_id
+  )
+}
+
+xlsx_read_workbook_rels <- function(path) {
+  ## do we really have to worry about this file not existing?
+  xml <- xlsx_read_file_if_exists(path, "xl/_rels/workbook.xml.rels")
+  if (is.null(xml)) {
+    return(NULL)
+  }
+  rel_nodes <- xml2::xml_children(xml)
+  rels <- rbind_df(lapply(rel_nodes, xml_attrs_list))
+  names(rels) <- tolower(names(rels))
+  rels
+  ## MAYBE TODOs, if decide to do more processing:
+  ##
+  ## prepend target with "xl/"
+  ## but check the type and don't to if an external reference
+  ##
+  ## just take the last bit of type, i.e.basename(type),
+}
+
+xlsx_read_worksheet_rels <- function(path) {
+  manifest <- xlsx_list_files(path)
+  holds_sheet_rels <-
+    grepl("xl/worksheets/_rels/sheet[0-9]*.xml.rels", manifest$Name)
+  if (none(holds_sheet_rels)) {
+    return(NULL)
+  }
+  sheet_rels_fnames <- manifest$Name[holds_sheet_rels]
+  nms <- gsub("xl/worksheets/_rels/(sheet[0-9]+).xml.rels", "\\1",
+              sheet_rels_fnames)
+  wr <- sheet_rels_fnames %>%
+    purrr::map(xlsx_read_file, path = path)
+  worksheet_rels <- wr %>%
+    purrr::map(xml2::xml_find_all, xpath = "//d1:Relationship") %>%
+    stats::setNames(nms)
+  f <- function(x) {
+    x %>%
+      purrr::map(xml2::xml_attrs) %>%
+      purrr::map(as.list) %>%
+      dplyr::bind_rows()
+  }
+  worksheet_rels %>%
+    purrr::map(f) %>%
+    dplyr::bind_rows(.id = "worksheet") %>%
+    stats::setNames(tolower(names(.)))
 }
 
 xlsx_read_workbook <- function(path) {
@@ -60,7 +150,7 @@ xlsx_ct_sheet <- function(xml, ns) {
     ref = attr_character(at[["id"]]))
 }
 
-## 18.14.6 definedName
+## 18.14.6 definedNames
 xlsx_ct_external_defined_names <- function(xml, ns) {
   process_container(xml, "d1:definedNames", ns, xlsx_ct_external_defined_name,
                     classes=TRUE)
